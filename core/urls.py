86dce9ac8243518d8f785c4a3067307317f92c35
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-10-06 16:32:22 +0500

    Make admin root url

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .views import health
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
     path("api/", include("api.urls")),
     path("health/", health, name="health"),
 ]
