e994ffc2d6b18af1d0f70044c8b090c4cd74ae26
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-11-27 14:31:51 +0500

    Try to fix admin media path

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 from .views import health
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("health/", health, name="health"),
-    path("", admin.site.urls),
 ]
 
 if settings.DEBUG:
