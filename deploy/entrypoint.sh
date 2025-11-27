4b10a8819ce31cca8bbafcf8dddcec6d50c9b3da
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-11-27 13:43:12 +0500

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,4 +2,9 @@
 set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers ${GUNICORN_WORKERS:-3} --log-file -
+exec gunicorn core.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers ${GUNICORN_WORKERS:-3} \
+    --access-logfile - \
+    --error-logfile - \
+    --log-level debug
